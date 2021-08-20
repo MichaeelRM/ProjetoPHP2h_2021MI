7f9e54c7cef9b1f286602895318211ffb936df6d
MichaeelRM <michelravi@outlook.com>
2021-08-19 22:14:02 -0300

    atualizando

---
+++ segundo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	#comentário
+	#Data: 19/08/21
+
+
+ ?>
+<head>
+	<meta charset="utf-8">
+	<title>PHP - Testando Comentário</title>
+</head>
+<body>
+	<?php 
+
+		/* 
+		Comentário de linha
+		
+		*/
+		echo "Segundo Exemplo De Comentário";
+		$data = "19 de agosto de 2021";
+	 ?>
+	 <p>Este paragrafo está sendo interpretado pelo html</p>
+	 Arquivo criado em php <?php echo $data; ?>
+	 
+</body>
+</html>
